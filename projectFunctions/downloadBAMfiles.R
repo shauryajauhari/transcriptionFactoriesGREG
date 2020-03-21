d7fc7427a44fb7698e86ffd9d96315b7f40adddd
Shaurya Jauhari <ishauryajauhari@gmail.com>
2020-03-22 03:59:56 +0800

    Change download.file() to curl_download()

--- projectFunctions/downloadBAMfiles.R
+++ projectFunctions/downloadBAMfiles.R
@@ -1,10 +1,14 @@
 downloadBAMfiles <- function(cell, feature)
 {
+  ## Install curl package
+  install.packages("curl")
+  library(curl)
+  
   if(cell %in% cells & feature %in% features)
   {
     for (lenList in 1:length(masterData$`Download Link`[masterData$`Cell Type`== cell & masterData$Feature == feature][[1]]))
     {
-      download.file(masterData$`Download Link`[masterData$`Cell Type`== cell & masterData$Feature == feature][[1]][lenList], 
+      curl::curl_download(masterData$`Download Link`[masterData$`Cell Type`== cell & masterData$Feature == feature][[1]][lenList], 
                     print(paste0(paste0("./GREG/",cell), paste0("/", feature), 
                                  paste0("/", basename(masterData$`Download Link`[masterData$`Cell Type`== cell
                                                                                  & masterData$Feature == feature][[1]][lenList])))))
