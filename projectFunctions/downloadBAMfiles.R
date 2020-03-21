97a89a027333d7a16fb1dbc546c0d28d6c36cf45
Shaurya Jauhari <ishauryajauhari@gmail.com>
2020-03-21 12:30:23 +0800

    Add code

---
+++ projectFunctions/downloadBAMfiles.R
@@ -0,0 +1,17 @@
+downloadBAMfiles <- function(cell, feature)
+{
+  if(cell %in% cells & feature %in% features)
+  {
+    for (lenList in 1:length(masterData$`Download Link`[masterData$`Cell Type`== cell & masterData$Feature == feature][[1]]))
+    {
+      download.file(masterData$`Download Link`[masterData$`Cell Type`== cell & masterData$Feature == feature][[1]][lenList], 
+                    print(paste0(paste0("./GREG/",cell), paste0("/", feature), 
+                                 paste0("/", basename(masterData$`Download Link`[masterData$`Cell Type`== cell
+                                                                                 & masterData$Feature == feature][[1]][lenList])))))
+    }
+  }
+  else
+  {
+    print("Invalid cell type or feature.")
+  }
+}
