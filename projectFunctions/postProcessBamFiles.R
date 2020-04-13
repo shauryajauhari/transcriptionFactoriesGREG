b39e905dc6dee45f23bcc6fab0ec5639b3ab84e2
Shaurya Jauhari <ishauryajauhari@gmail.com>
2020-04-13 13:48:52 +0800

    Exclude package installation command

--- projectFunctions/postProcessBamFiles.R
+++ projectFunctions/postProcessBamFiles.R
@@ -5,86 +5,85 @@
 postProcessBamFiles <- function(cell)
   {
   
-  # Listing of all "readCounts.tab" from each feature.
+    # Listing of all "readCounts.tab" from each feature.
   
-  covFiles <- list.files(path=paste0("./GREG/", cell, "/"), pattern = ".tab", recursive = TRUE)
+    covFiles <- list.files(path=paste0("./GREG/", cell, "/"), pattern = ".tab", recursive = TRUE)
 
-  # Loading those files to a list of dataframes.
+    # Loading those files to a list of dataframes.
   
-dataCovFiles <- lapply(covFiles, function(path) {
-  read.delim( file = paste0("./GREG/", cell, "/", path), header = TRUE, sep = "\t")
-})
+    dataCovFiles <- lapply(covFiles, function(path) {
+      read.delim( file = paste0("./GREG/", cell, "/", path), header = TRUE, sep = "\t")
+      })
 
-  # Naming list indices for intuitive access.
+     # Naming list indices for intuitive access.
 
-names(dataCovFiles) <- sub("\\/.*", "", covFiles)
+    names(dataCovFiles) <- sub("\\/.*", "", covFiles)
 
-  # Calculating the average of read counts from all replicates present.
+    # Calculating the average of read counts from all replicates present.
 
-avgReads <- lapply(dataCovFiles, function(avgDf){
-  return(cbind(avgDf, rowMeans(avgDf[,-c(1:3)]))) # leaving out the first three columns, i.e. chr, start, end.
-})
+    avgReads <- lapply(dataCovFiles, function(avgDf){
+      return(cbind(avgDf, rowMeans(avgDf[,-c(1:3)]))) # leaving out the first three columns, i.e. chr, start, end.
+    })
 
-  # Trimming the dataframes for relevant columns only.
+    # Trimming the dataframes for relevant columns only.
 
-selectAvgReads <- lapply(avgReads, function(d){
-  subset(d, select = c("X..chr.", "X.start.", "X.end.", "rowMeans(avgDf[, -c(1:3)])"))
-})
-
-
-## Here we diverge. 
+    selectAvgReads <- lapply(avgReads, function(d){
+      subset(d, select = c("X..chr.", "X.start.", "X.end.", "rowMeans(avgDf[, -c(1:3)])"))
+    })
 
 
-  # (i) For each cell-type.
-  # Merge all dataframes first and then remove the "chr", "start", and "end" columns.
+    ## Here we diverge.
 
-install.packages("plyr")
-library(plyr)
 
-combinedAvgReads <- join_all(selectAvgReads, by=c("X..chr.", "X.start.", "X.end."), type='left')
+    # (i) For each cell-type.
+    # Merge all dataframes first and then remove the "chr", "start", and "end" columns.
+
+  library(plyr)
+
+  combinedAvgReads <- join_all(selectAvgReads, by=c("X..chr.", "X.start.", "X.end."), type='left')
 
   # Since we no longer need the bins' attributes, we omit them here. Now we're left with the read counts for
   # all features, against each given feature.
 
-combinedAvgReads <- combinedAvgReads[,-c(1:3)]
-colnames(combinedAvgReads) <- names(dataCovFiles)
+  combinedAvgReads <- combinedAvgReads[,-c(1:3)]
+  colnames(combinedAvgReads) <- names(dataCovFiles)
 
   # Replacing NAs induced by combining dataframes by 0.
-combinedAvgReads[is.na(combinedAvgReads)] <- 0
+  combinedAvgReads[is.na(combinedAvgReads)] <- 0
 
   # Normalizing reads by BPM.
-normAvgReads <- as.data.frame(sapply(combinedAvgReads, bpmNormalize))
+  normAvgReads <- as.data.frame(sapply(combinedAvgReads, bpmNormalize))
 
   # Save file and write as output.
 
-write.table(normAvgReads, file = paste0("./GREG/", cell, "/normalizedReads.txt") , sep = "\t", row.names = FALSE)
+  write.table(normAvgReads, file = paste0("./GREG/", cell, "/normalizedReads.txt") , sep = "\t", row.names = FALSE)
 
 
  # (ii) For each feature
 
-individScores <- selectAvgReads
+  individScores <- selectAvgReads
 
-for (i in 1:length(individScores))
-  {
-    # BPM Normalize.
-    individScores[[i]]$normReads <- bpmNormalize(individScores[[i]]$`rowMeans(avgDf[, -c(1:3)])`)
+  for (i in 1:length(individScores))
+    {
+      # BPM Normalize.
+      individScores[[i]]$normReads <- bpmNormalize(individScores[[i]]$`rowMeans(avgDf[, -c(1:3)])`)
   
-    # 1-index the BED file
-    individScores[[i]][2] <- individScores[[i]][2] + 1
+      # 1-index the BED file
+      individScores[[i]][2] <- individScores[[i]][2] + 1
   
-    # Remove raw read counts 
-    individScores[[i]][4] <- c()
+      # Remove raw read counts 
+      individScores[[i]][4] <- c()
   
-  # Save and write the respective bedGraph file.
+      # Save and write the respective bedGraph file.
   
-  write.table(individScores[[i]],
-              file = paste0("./GREG/", cell, "/", names(individScores[i]), "/", "normalizedReads.bedGraph"),
-              sep = "\t",
-              quote = FALSE,
-              row.names = FALSE,
-              col.names = FALSE)
+      write.table(individScores[[i]],
+                  file = paste0("./GREG/", cell, "/", names(individScores[i]), "/", "normalizedReads.bedGraph"),
+                  sep = "\t",
+                  quote = FALSE,
+                  row.names = FALSE,
+                  col.names = FALSE)
   
-  }
+    }
 }
 
 
