ea8733834df3e7899b3bf139a68d12e85c9e58ce
Shaurya Jauhari <shauryajauhari@gzhmu.edu.cn>
2020-09-08 15:17:40 +0800

    Remove file

--- projectFunctions/tests/test_bpmNormalize.R
+++
@@ -1,6 +0,0 @@
-context("BPM Normalization")
-
-test_that("Result is a float", 
-          {
-            expect_is(bpmNormalize(13.0), "numeric") 
-          }) 
--- projectFunctions/tests/test_checkingBAMDownload.R
+++
@@ -1,4 +0,0 @@
-context("BAM file downloads") 
-
-test_that("Download exists", {expect_output(checkingBAMDownload("A549", "CTCF"), "Successfully Downloaded!") })
-test_that("Download exists", {expect_output(checkingBAMDownload("A549", "NANA"), "Invalid cell-type or feature.") })
