a11315b3f85a079a2619c50880079694bba94334
Shaurya Jauhari <shauryajauhari@gzhmu.edu.cn>
2020-09-08 13:37:08 +0800

    Revise code

--- projectFunctions/downloadBAMfiles.R
+++ projectFunctions/downloadBAMfiles.R
@@ -1,21 +1,21 @@
 downloadBAMfiles <- function(cell, feature)
 {
-  ## Install "curl" package and load library
+  ## Install "curl" package (if absent) and load library
   if (!require(curl)) install.packages("curl", dependencies = TRUE)
   library(curl)
   
   if(cell %in% cells & feature %in% features)
   {
-    for (lenList in 1:length(masterData$`Download Link`[masterData$`Cell Type`== cell & masterData$Feature == feature][[1]]))
+    for (lenList in 1:length(unlist(strsplit(masterData$`Download Link`[masterData$`Cell Type`== cell & masterData$Feature == feature][[1]], ","))))
     {
-      curl_download(masterData$`Download Link`[masterData$`Cell Type`== cell & masterData$Feature == feature][[1]][lenList], 
+      curl_download(trimws(unlist(strsplit(masterData$`Download Link`[masterData$`Cell Type`== cell & masterData$Feature == feature][[1]], ","))[lenList]), 
                     print(paste0(paste0("./GREG/",cell), paste0("/", feature), 
-                                 paste0("/", basename(masterData$`Download Link`[masterData$`Cell Type`== cell
-                                                                                 & masterData$Feature == feature][[1]][lenList])))))
+                                 paste0("/", basename(unlist(strsplit(masterData$`Download Link`[masterData$`Cell Type`== cell
+                                                                               & masterData$Feature == feature][[1]], ","))[lenList])))))
     }
   }
   else
   {
-    print("Invalid cell type or feature.")
+    print("Invalid cell-type or feature.")
   }
 }
