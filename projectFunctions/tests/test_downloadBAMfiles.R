d2dbc44f41a92302f9704242da248724f9b0ba60
Shaurya Jauhari <ishauryajauhari@gmail.com>
2020-09-08 11:16:31 +0800

    Update file

--- projectFunctions/tests/test_downloadBAMfiles.R
+++ projectFunctions/tests/test_downloadBAMfiles.R
@@ -1,4 +1,10 @@
 context("BAM file downloads") 
-#test_that("Inputs are strings", {expect_type(downloadBAMfiles(cell, feature), "character", "character") }) 
 
-test_that("Output", {expect_output(downloadBAMfiles(cell, feature), ) }) 
+## Loading background information
+masterData <- read_excel("siftedData.xlsx")
+cells <- c("IMR90", "MCF7", "HELA", "K562", "H1ESC", "A549")
+features <- c("CTCF", "EP300", "H3K27me3", "H3K36me3", "H3K4me1", "H3K4me2", 
+             "H3K4me3", "H3K9ac", "H3K9me3", "RAD21", "RNAPol2", "RNAPol3", "RNA-Seq", "YY1")
+
+
+test_that("Download exists", {expect_output(downloadBAMfiles("A549", "CTCF"), TRUE) }) 
