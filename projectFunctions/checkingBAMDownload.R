ee539f73556bb5e61b594aec04719018b7acd87d
Shaurya Jauhari <shauryajauhari@gzhmu.edu.cn>
2020-09-08 15:17:11 +0800

    Update file

--- projectFunctions/checkingBAMDownload.R
+++ projectFunctions/checkingBAMDownload.R
@@ -1,5 +1,12 @@
 checkingBAMDownload <- function(cell, feature)
 {
+  
+  cells <- c("A549", "H1ESC", "HELA", "IMR90", "K562", "MCF7")
+  features <- c("CTCF", "EP300", "H3K27me3", "H3K36me3", "H3K4me1", "H3K4me2", "H3K4me3", "H3K9ac", "H3K9me3", "RAD21", "RNAPol2", "RNAPol3", "RNA-Seq", "YY1")
+  
+  library(readxl) 
+  # masterData <- read_excel(paste0(getwd(),"/siftedData.xlsx"))
+
 
   if(cell %in% cells & feature %in% features)
   {
@@ -8,7 +15,7 @@ checkingBAMDownload <- function(cell, feature)
     count = 0
   
     ## List the BAM files  
-    count <- length(list.files(paste0(paste0("./GREG/",cell), paste0("/", feature, "/"))))
+    count <- length(list.files(paste0(getwd(),"/GREG/",cell, "/", feature, "/")))
   
     ## Total Number of files as listed in the master table
     totalFiles <- length(unlist(strsplit(masterData$`Download Link`[masterData$`Cell Type`== cell & masterData$Feature == feature][[1]], ",")))
@@ -20,7 +27,7 @@ checkingBAMDownload <- function(cell, feature)
   
   else
   {
-    print("Invalid cell-type or feature.")
+    return("Invalid cell-type or feature.")
   }
 
 }
