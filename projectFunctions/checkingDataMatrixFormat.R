7d1df75a6f82ce7fe24a43269c020e9d12cd6492
Shaurya Jauhari <ishauryajauhari@gmail.com>
2020-09-09 09:57:32 +0800

    Provision check for non-NA entries in the data

--- projectFunctions/checkingDataMatrixFormat.R
+++ projectFunctions/checkingDataMatrixFormat.R
@@ -13,8 +13,10 @@ checkingDataMatrixFormat <- function(cell){
     ## Number of folders for features
     numberFolders <- length(list.dirs(path = paste0(getwd(), "/GREG/", cell, "/")))
     
-    ## Condition checking | have all features been covered in columns and appropriate rows exist?
-    ifelse(numberColumns == numberFolders & nrow(covFile)>1, return("All good!"), return("Ouch! Did you miss something?"))
+    ## Condition checking | Have all features been covered in columns, appropriate rows exist (i.e. greater than 1),
+    ## and no NA value exists in the data (i.e. just one factor level : FALSE)?
+    ifelse(numberColumns==numberFolders & nrow(covFile)>1 & length(levels(factor(is.na.data.frame(covFile))))==1, 
+           return("All good!"), return("Ouch! Did you miss something?"))
   }
   
   else
