b3a9069c1df0bf090befc50baf7d700481881b66
Shaurya Jauhari <shauryajauhari@gzhmu.edu.cn>
2020-09-15 15:20:34 +0800

    Add check for missing arguments

--- projectFunctions/downloadBAMfiles.R
+++ projectFunctions/downloadBAMfiles.R
@@ -4,11 +4,25 @@
 
 downloadBAMfiles <- function(cell, feature)
 {
-  ## Install packages (if absent) and loading libraries
+  ## Check if any or both arguments are missing.
   
-  if (!require(c('readxl','curl'))) install.packages(c("curl", "readxl"), dependencies = TRUE)
-  library(curl)
+  if(missing(cell) | missing(feature))
+  {
+    return("Argument missing.")
+  }
+  
+  
+  ## Install packages (if absent)
+  
+  requiredPackages <- c("curl", "readxl")
+  newPackages <- requiredPackages[!(requiredPackages %in% installed.packages()[,"Package"])]
+  if(length(newPackages)) install.packages(newPackages)
+
+  ## Loading package libraries
+ 
+   library(curl)
   library(readxl) 
+  
   
   ## Loading metadata table
   
@@ -19,6 +33,7 @@ downloadBAMfiles <- function(cell, feature)
   
   cells <- c("A549", "H1ESC", "HELA", "IMR90", "K562", "MCF7")
   features <- c("CTCF", "EP300", "H3K27me3", "H3K36me3", "H3K4me1", "H3K4me2", "H3K4me3", "H3K9ac", "H3K9me3", "RAD21", "RNAPol2", "RNAPol3", "RNA-Seq", "YY1")
+  
   
   if(cell %in% cells & feature %in% features)
   {
@@ -33,6 +48,6 @@ downloadBAMfiles <- function(cell, feature)
   }
   else
   {
-    print("Invalid cell-type or feature.")
+    return("Invalid cell-type or feature.")
   }
 }
