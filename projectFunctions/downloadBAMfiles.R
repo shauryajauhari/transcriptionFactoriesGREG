972c2714317ff02179a10d3462f376a281c36097
Shaurya Jauhari <shauryajauhari@gzhmu.edu.cn>
2020-09-15 14:46:11 +0800

    Update file

--- projectFunctions/downloadBAMfiles.R
+++ projectFunctions/downloadBAMfiles.R
@@ -1,17 +1,34 @@
+## Author : Shaurya Jauhari
+## Last Reviewed: September 15th, 2020.
+## Description: Download BAM files
+
 downloadBAMfiles <- function(cell, feature)
 {
-  ## Install "curl" package (if absent) and load library
-  if (!require(curl)) install.packages("curl", dependencies = TRUE)
+  ## Install packages (if absent) and loading libraries
+  
+  if (!require(c('readxl','curl'))) install.packages(c("curl", "readxl"), dependencies = TRUE)
   library(curl)
+  library(readxl) 
+  
+  ## Loading metadata table
+  
+  masterData <- read_excel(paste0(getwd(),"/siftedData.xlsx"))
+  
+  
+  ## Loading definitions
+  
+  cells <- c("A549", "H1ESC", "HELA", "IMR90", "K562", "MCF7")
+  features <- c("CTCF", "EP300", "H3K27me3", "H3K36me3", "H3K4me1", "H3K4me2", "H3K4me3", "H3K9ac", "H3K9me3", "RAD21", "RNAPol2", "RNAPol3", "RNA-Seq", "YY1")
   
   if(cell %in% cells & feature %in% features)
   {
     for (lenList in 1:length(unlist(strsplit(masterData$`Download Link`[masterData$`Cell Type`== cell & masterData$Feature == feature][[1]], ","))))
     {
-      curl_download(trimws(unlist(strsplit(masterData$`Download Link`[masterData$`Cell Type`== cell & masterData$Feature == feature][[1]], ","))[lenList]), 
-                    print(paste0(paste0("./GREG/",cell), paste0("/", feature), 
+      curl_download(url = trimws(unlist(strsplit(masterData$`Download Link`[masterData$`Cell Type`== cell & masterData$Feature == feature][[1]], ","))[lenList]),
+                    destfile = print(paste0(paste0("./GREG/",cell), paste0("/", feature), 
                                  paste0("/", basename(unlist(strsplit(masterData$`Download Link`[masterData$`Cell Type`== cell
-                                                                               & masterData$Feature == feature][[1]], ","))[lenList])))))
+                                                                               & masterData$Feature == feature][[1]], ","))[lenList])))),
+                    quiet = FALSE)
     }
   }
   else
