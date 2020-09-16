703ab0a4b49536994e72b94c4531e61497075595
Shaurya Jauhari <shauryajauhari@gzhmu.edu.cn>
2020-09-16 14:45:51 +0800

    Remove file

--- projectFunctions/tests/testthat/test_checkingBAMDownload.R
+++
@@ -1,7 +0,0 @@
-context("BAM file downloads") 
-
-## Check this after you have successfully downloaded the BAM files 
-test_that("Download exists", {expect_identical(checkingBAMDownload("A549", "CTCF"), "Successfully Downloaded!") })
-
-test_that("Invalid input", {expect_identical(checkingBAMDownload("A549", "NANA"), "Invalid cell-type or feature.") })
-test_that("Download absent", {expect_identical(checkingBAMDownload("A549", "YY1"), "Missing files. Download Error. Please check!") })
