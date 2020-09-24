633a1b95b84fc02a8a622b3a1b0d92f6b8fa4a9b
Shaurya Jauhari <shauryajauhari@gzhmu.edu.cn>
2020-09-24 16:06:58 +0800

    Update code

--- MachineLearning/modelPerformance.R
+++ MachineLearning/modelPerformance.R
@@ -53,10 +53,10 @@ modelPerformance <- function (model, testData, testDataClass) {
 
 ## Statistical Significance of the model (linear models only)
   
-  ifelse(class(model) %in% c("glm","lm"), overallP <- 1- (pchisq(eval(parse(text= paste0(deparse(substitute(model)), "$null.deviance"))) - eval(parse(text= paste0(deparse(substitute(model)), "$deviance"))), 
-                                                                 eval(parse(text= paste0(deparse(substitute(model)), "$df.null"))) - eval(parse(text= paste0(deparse(substitute(model)), "$df.residual"))),
-                                                                 lower.tail = FALSE)), )
-  
+  # ifelse(class(model) %in% c("glm","lm"), overallP <- 1- (pchisq(eval(parse(text= paste0(deparse(substitute(model)), "$null.deviance"))) - eval(parse(text= paste0(deparse(substitute(model)), "$deviance"))),
+  #                                                                eval(parse(text= paste0(deparse(substitute(model)), "$df.null"))) - eval(parse(text= paste0(deparse(substitute(model)), "$df.residual"))),
+  #                                                                lower.tail = FALSE)), )
+  ifelse(class(model) %in% c("glm","lm"), overallP <- with(model, pchisq(null.deviance - deviance, df.null - df.residual, lower.tail = FALSE)), )
   cat("The statistical significance for the model is", overallP, "\n")
   cat("The confidence level for the model is",((1-overallP)*100), "%")
     
