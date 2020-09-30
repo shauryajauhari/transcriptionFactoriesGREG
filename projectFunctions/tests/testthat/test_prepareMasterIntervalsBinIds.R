16e14022e3e587b88e38bb31360c748dd9a8c622
Shaurya Jauhari <shauryajauhari@gzhmu.edu.cn>
2020-09-30 12:42:59 +0800

    Revise typo

--- projectFunctions/tests/testthat/test_prepareMasterIntervalsBinIds.R
+++ projectFunctions/tests/testthat/test_prepareMasterIntervalsBinIds.R
@@ -3,7 +3,7 @@ context("Scaling BED intervals with GREG indices")
 
 test_that("Unsuccessful execution", {expect_match(prepareMasterIntervalsBinIds("notMe.bed"), "File not found. Please check!") })
 # test_that("Successful execution", {expect_match(prepareMasterIntervalsBinIds("hg19_2k_bins.bed"), "File saved successfully!") })
-test_that("Expect that output is a list", {expect_s3_class(prepareMasterIntervalsBinIds("hg19_2k_bins.bed"), "data.frame") })
+test_that("Expect that output is a data-frame", {expect_s3_class(prepareMasterIntervalsBinIds("hg19_2k_bins.bed"), "data.frame") })
 test_that("Expect greater than a row", {expect_gte(nrow(prepareMasterIntervalsBinIds("hg19_2k_bins.bed")), 1)})
 test_that("Expect five columns in total", {expect_equal(ncol(prepareMasterIntervalsBinIds("hg19_2k_bins.bed")), 5)})
 test_that("Expect column names as chr, start, and end", {expect_identical(colnames(prepareMasterIntervalsBinIds("hg19_2k_bins.bed"))[1:3], c("chr", "start", "end"))})
