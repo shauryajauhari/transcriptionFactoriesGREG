32879952d50785c76a10159ef1621699b0925385
Shaurya Jauhari <shauryajauhari@gzhmu.edu.cn>
2020-10-15 11:08:56 +0800

    Update annotation

--- MachineLearning/R/dataSimulation.R
+++ MachineLearning/R/dataSimulation.R
@@ -1,5 +1,5 @@
 ## Author : Shaurya Jauhari
-## Last Reviewed: October 14th, 2020.
+## Last Reviewed: October 15th, 2020.
 ## Description: This function takes a feature-file, a chromosome-name, and a feature-name as arguments and 
 ## adds random values between the maximum value and twice the maximum value of the read-coverages of that 
 ## feature. This distorts proportion of that feature to the overall classification model and might be useful
@@ -21,21 +21,23 @@ dataSimulation <- function(dataFile, chrName, featureName){
   
   ## Extracting chromosome-specific data
   
-  stopifnot(chrName %in% unique(standardData$chr))
+  stopifnot(chrName %in% unique(standardData$chr)) ## check for a valid chromosome
   chrData <- standardData[standardData$chr == chrName,]
   
   ## Locating feature data to be tampered
   
-  stopifnot(featureName %in% colnames(chrData))
+  stopifnot(featureName %in% colnames(chrData)) ## check for a valid column-name
   selectedFeature <- chrData[ , featureName] 
   
   
   ### SIMULATION ###
-  ## The idea is to add exorbitant values to the base values to the selected feature (read-coverages)
+  ## The idea is to add exorbitant values to the base values to the selected feature (read-coverage)
   ## that is the most influential variable to the model. For the same, we plan on adding random values
   ## between the original maximum value of the coverage and twice that number.
   
+  ## Generate random numbers in the mentioned range
   selectedFeature <- runif(length(selectedFeature), min = max(selectedFeature), max = 2 * max(selectedFeature))
+  
   chrData[ , featureName] <- selectedFeature
   return(chrData)
 }
