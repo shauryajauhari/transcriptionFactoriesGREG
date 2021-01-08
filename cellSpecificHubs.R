83735361547dd8d46694f7131f5a3d38f2ff90f1
Shaurya Jauhari <shaurya126906@st.jmi.ac.in>
2021-01-08 20:52:28 +0800

    Update file

--- cellSpecificHubs.R
+++ cellSpecificHubs.R
@@ -12,7 +12,7 @@ onlyData <- Long_Range_relationships[, c(1,6)]
 names(onlyData) <- c("LRIDs", "celltype")
 
 ## Merge data frames based on LRIDs (common column)
-jointSet <- merge(temp, mapped, by="LRIDs")
+jointSet <- merge(onlyData, mapped, by="LRIDs")
 
 ## Sifting cell-specific bin-data
 
@@ -23,20 +23,49 @@ for(cell in cells)
   assign(paste0(cell,"Data"), jointSet[jointSet$celltype == cell, 6])
 }
 
-fullData <- list(A549 <- A549Data, 
-                 H1ESC <- H1ESCData, 
-                 HeLa <- HELAData, 
-                 IMR90 <- IMR90Data, 
-                 K562 <- K562Data, 
-                 MCF7 <- MCF7Data)
+fullData <- list(A549Data, H1ESCData, HELAData, IMR90Data, K562Data, MCF7Data)
+names(fullData) <- c("A549", "H1ESC", "HeLa", "IMR90", "K562", "MCF7")
+
+## Plotting cell-wise frequency
+freq <- vector(mode = "list", length = length(fullData))
+for(i in 1:length(fullData)) freq[i] <- length(fullData[[i]])
+
+barplot(as.numeric(freq),
+main = "Number of Hubs in each Cell",
+xlab = "Cell",
+ylab = "Number of Hubs",
+names.arg = c("A549", "H1ESC", "HeLa", "IMR90", "K562", "MCF7"),
+col = "darkred",
+horiz = FALSE)
+
+## basic barplot
+forPlot <- data.frame(c("A549", "H1ESC", "HeLa", "IMR90", "K562", "MCF7"),
+                       as.numeric(freq))  
+
+## using Plotly
+library(plotly)
+fig <- plot_ly(x = c("A549", "H1ESC", "HeLa", "IMR90", "K562", "MCF7") ,                                  
+        y = as.numeric(freq),
+        type = "bar", color = I("orange"))
+        
+fig <- fig %>% layout(title = "Hub Frequencies in Cells",
+                      yaxis = list(title = 'Number of Hubs'),
+                      xaxis = list(title = 'Cell'))
+fig
+
+##using ggplot2
+library(ggplot2)
+ggplot(forPlot, aes(x = c("A549", "H1ESC", "HeLa", "IMR90", "K562", "MCF7"),
+                    y = as.numeric(freq))) +        
+  geom_bar(stat = "identity")
+
 
 ## Creating a Venn Diagram
-'/
-//'
 ## Installing and Loading Packages
 
-if (!require(devtools)) install.packages("devtools")
-devtools::install_github("yanlinlin82/ggvenn")
-library(ggvenn)
+library(devtools)
+install_github("js229/Vennerable")
+library(Vennerable)
 
-ggvenn(fullData)
+myVenn <- Venn(fullData)
+plot(myVenn, doWeights = FALSE)
